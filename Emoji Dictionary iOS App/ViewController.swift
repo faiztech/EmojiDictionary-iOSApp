d576fe8a4b6d0120fb6fc5f3c38ab3e4d51fd669
Mohammed Faiz <mfaizudd@neiu.edu>
2017-09-09 12:38:30 -0500

    Initial Commit

---
+++ Emoji Dictionary iOS App/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Emoji Dictionary iOS App
+//
+//  Created by Mohammed Faizuddin on 9/9/17.
+//  Copyright © 2017 Mohammed Faizuddin. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
